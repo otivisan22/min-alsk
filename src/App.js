d370d985dd7b0aa0854152ec8c2af4b1338ae73c
otivisan22 <71434012+otivisan22@users.noreply.github.com>
2021-11-29 20:49:24 +0000

    added new components/set up routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import { lightTheme, darkTheme, GlobalStyles } from "./theme.js";
+import NavigationBar from "./components/NavigationBar";
+import Routes from "./Routes";
+
+import "./App.css";
+
+const client = new ApolloClient({
+  uri: process.env.GRAPHQL_URL || "http://localhost:3000/",
+  cache: new InMemoryCache(),
+});
+
+const StyledApp = styled.div`
+  color: ${(props) => props.theme.fontColor};
+`;
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const themeToggler = () => {
+    theme === `light` ? setTheme(`dark`) : setTheme(`light`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles></GlobalStyles>
+      <StyledApp>
+        <ApolloProvider client={client}>
+          <Router>
+            <NavigationBar />
+            <Routes />
+          </Router>
+        </ApolloProvider>
+        Hej
+        <button onClick={() => themeToggler()}></button>
+      </StyledApp>{" "}
+      */
+    </ThemeProvider>
   );
 }
 
